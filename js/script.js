b8b9f3a538e396196b96df234b91b33a4adece59
Wojciech Klama <wojciechklamaczp@gmail.com>
2025-09-12 13:26:25 +0200

    format functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 {
-console.log("Pozdrowienia");
+const welcome = () => {
+  console.log("Pozdrowienia");
+};
 
-let navigation__switchModeButton = document.querySelector(".navigation__switchModeButton");
-
-
-navigation__switchModeButton.addEventListener("click", () => {
-  let body = document.querySelector(".body");
-  let navigation__buttonText = document.querySelector(".navigation__buttonText");
+const toggleBackGrundButton = () => {
+  const body = document.querySelector(".body");
+  const navigation__buttonText = document.querySelector(".navigation__buttonText");
   const isDark = body.classList.toggle("bodyDark");
   navigation__buttonText.innerText = isDark ? "Włącz Jasny Motyw" : "Włącz Ciemny Motyw";
-});
+};
+
+const init = () => {
+  const navigation__switchModeButton = document.querySelector(".navigation__switchModeButton");
+  navigation__switchModeButton.addEventListener("click", toggleBackGrundButton);
+}
+
+  welcome();
+  init();
 }
